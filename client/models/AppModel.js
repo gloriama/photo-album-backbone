3dbf7f15be89fb797f69c92cfc551ee41007bcce
Gloria Ma <gloria.ma@aya.yale.edu>
2016-04-01 09:38:19 -0700

    Renders current photo

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -1,5 +1,6 @@
 var AppModel = Backbone.Model.extend({
   initialize: function(params) {
     this.set('photos', params.photos);
+    this.set('currentPhoto', params.photos[0]);
   }
 });
--- client/views/AppView.js
+++ client/views/AppView.js
@@ -3,19 +3,29 @@ var AppView = Backbone.View.extend({
     this.render();
   },
 
-  template: _.template('<tr><td><%= photo.title %></td></tr>'),
+  selectorTemplate: _.template('<tr><td><%= photo.title %></td></tr>'),
+
+  viewPortTemplate: _.template('<p><%= currentPhoto.title %></p><img src="<%= currentPhoto.url %>" />'),
 
   render: function() {
+    // Selector
     var selector = $('<table>');
     var that = this;
     selector.html(this.model.get('photos').map(function(photo) {
-      return that.template({
+      return that.selectorTemplate({
         photo: photo
       });
     }));
 
+    // ViewPort
+    var viewPort = $('<div>');
+    viewPort.html(this.viewPortTemplate({
+      currentPhoto: this.model.get('currentPhoto')
+    }));
+
     this.$el.html('<p>Photo Album</p>');
     this.$el.append(selector);
+    this.$el.append(viewPort);
 
     return this.$el;
   }
