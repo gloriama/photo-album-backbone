02919d4113584e057daadaff9e93cf578fc0b8d7
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-31 22:30:22 -0700

    Renders photo titles

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -3,12 +3,20 @@ var AppView = Backbone.View.extend({
     this.render();
   },
 
-  template: _.template('<p>Photo Album</p><p><%= photos[0].title %></p>'),
+  template: _.template('<tr><td><%= photo.title %></td></tr>'),
 
   render: function() {
-    this.$el.html(this.template({
-      photos: this.model.get('photos')
+    var selector = $('<table>');
+    var that = this;
+    selector.html(this.model.get('photos').map(function(photo) {
+      return that.template({
+        photo: photo
+      });
     }));
+
+    this.$el.html('<p>Photo Album</p>');
+    this.$el.append(selector);
+
     return this.$el;
   }
 });
